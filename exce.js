38f2cf5324e3e853ce924dc9a1f5c70b9c9cab3e
Jasper <jaspereffiong27@gmail.com>
2026-03-04 12:40:37 +0100

    added  2 more functions

--- exce.js
+++ exce.js
@@ -22,5 +22,37 @@ function isEven(number) {
   console.log(isEven(4))
 
   // Arrow Function
+ // Example 1
+  const divide =(num3, num4) => {
+    return num3 / num4;
+  };
+   console.log(divide(40,4))
 
-  
+// Example 2
+let greetUser = (name) => {
+    return "Welcome " + name;
+};
+ console.log(greetUser("Ferdrick"));
+
+ // Functions calling functions
+
+ function add(a, b){
+    return a + b;
+ }
+
+  function addAndMultiply(a, b) {
+    let sum =add(a, b);
+    return sum * 2;
+  }
+   console.log(addAndMultiply(3, 4));
+
+   // e.g 2
+   function greet( name) {
+    return "Hello " + name;
+   }
+
+   function sayGoodbye(name) {
+    let greeting = greet(name);
+    return greeting + "  Goodbye"
+   }
+    console.log(sayGoodbye(' Able'));
